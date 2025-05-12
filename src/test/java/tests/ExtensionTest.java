8f500c965db78a3c4340e786572a5ef6b4cc81c0
okulovmaxim <okulov.m.i@gmail.com>
2025-05-12 16:22:07 +0500

    Пример реализации интерфейсов AfterTestExecutionCallback и AfterAllCallback

--- src/test/java/tests/ExtensionTest.java
+++ src/test/java/tests/ExtensionTest.java
@@ -1,17 +1,24 @@
 package tests;
 
+import callbacks.TestSaver;
 import models.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import parameters.RandomUser;
 import parameters.RandomUserResolver;
 
-@ExtendWith(RandomUserResolver.class)
+@ExtendWith({RandomUserResolver.class, TestSaver.class})
 public class ExtensionTest {
 
     @Test
     public void testWithParam(@RandomUser User user) {
         System.out.println(user.getFirstName());
         System.out.println(user.getLastName());
+    }
+
+    @Test
+    public void failedTest() {
+        Assertions.assertEquals(3,5);
     }
 }
